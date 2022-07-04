2b39c8f60e857c09ef09f0736bf54c5ea62f85ad
RafaRed <rafaelszcardoso@gmail.com>
2022-07-04 15:23:12 -0300

    Project Base

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,3 +1,37 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const { initializeApp } = require("firebase-admin/app");
+var admin = require("firebase-admin");
+var serviceAccount = require("./config.json");
+const rateLimit = require("express-rate-limit");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://greenchaindb-default-rtdb.firebaseio.com"
+  });
+
+  //initializeApp();
+var db = admin.database();
+const app = express();
+app.use(cors());
+app.use(
+	rateLimit({
+		windowMs: 1 * 60 * 60 * 1000, // 12 hour duration in milliseconds
+		max: 150,
+		message: "You exceeded requests hour limit!",
+		headers: true,
+	})
+);
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
+
+app.post("/", (req, res) => {
+    
+});
+
+exports.app = functions.https.onRequest(app);
