40e2e340678f1ffec9c8c55214b37d7f3f52ec10
RafaRed <rafaelszcardoso@gmail.com>
2022-07-07 18:11:21 -0300

    server connecting, loading and saving data on ipfs

--- server/functions/index.js
+++ server/functions/index.js
@@ -5,13 +5,22 @@ const { initializeApp } = require("firebase-admin/app");
 var admin = require("firebase-admin");
 var serviceAccount = require("./config.json");
 const rateLimit = require("express-rate-limit");
+const {
+	getAccessToken,
+	makeStorageClient,
+	uploadStringToJsonFile,
+	retrieveFile,
+	getName,
+	publishContent,
+	generateNameKey,
+} = require("./model/ipfs");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://greenchaindb-default-rtdb.firebaseio.com"
-  });
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://greenchaindb-default-rtdb.firebaseio.com",
+});
 
-  //initializeApp();
+//initializeApp();
 var db = admin.database();
 const app = express();
 app.use(cors());
@@ -29,9 +38,46 @@ app.use(function (req, res, next) {
 	next();
 });
 
+app.post("/ipfs-post", (req, res) => {
+	var _data = req.body.data;
+	console.log(_data);
+	var _filename =
+		req.body.filename !== undefined ? req.body.filename : "file.json";
+	var cid = uploadStringToJsonFile(_data, _filename);
+	cid.then((file_cid) => {
+		res.send(file_cid);
+	});
+});
 
-app.post("/", (req, res) => {
-    
+app.post("/ipfs-get", (req, res) => {
+	var _cid = req.body.cid;
+	var _filename =
+		req.body.filename !== undefined ? req.body.filename : "file.json";
+	var file = retrieveFile(_cid, _filename);
+	file.then((content) => {
+		res.send(content);
+	});
+});
+
+app.post("/root", (req, res) => {
+	const name = "k51qzi5uqu5dj3yedzwblg1mzwf518rngk4o345oab5m69pthwt8ptjxsm4m5o"
+	const value = "test"
+	publishContent(name,value).then((result) => {
+		res.send(result);
+	});
+	
+});
+
+app.post("/getname", (req, res) => {
+	getName().then((result) => {
+		res.send(result);
+	});
+	
+});
+
+app.post("/generatekey", (req, res) => {
+	//generateNameKey();
+	//res.send("done")
 });
 
 exports.app = functions.https.onRequest(app);
