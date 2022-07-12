abe45074abdeb73820a3b96866e50180a7bc7646
Luciano Ferreira <lfbnwork@gmail.com>
2022-07-12 09:07:15 +0200

    layout new report

---
+++ web/src/components/js/Divider.js
@@ -0,0 +1,4 @@
+import '../css/Divider.css';
+export function Divider() {
+    return (<div className='divider'></div>);
+}
--- web/src/pages/js/Newreport.js
+++ web/src/pages/js/Newreport.js
@@ -4,6 +4,9 @@ import { NavBar } from '../../components/js/NavBar';
 import { Label } from '../../components/js/Label';
 import { Title } from '../../components/js/Title';
 import { InputText } from '../../components/js/InputText';
+import { PrimaryButton } from '../../components/js/PrimaryButton';
+import { Divider } from '../../components/js/Divider';
+
 
 function Newreport() {
     return (
@@ -21,10 +24,64 @@ function Newreport() {
 
                 <Label label={'Description'}></Label>
 
-                <textarea placeholder="" rows="4" cols="50"> </textarea>
+                <div className='textarea-frame'>
+                    <textarea placeholder="" rows="4" cols="50"> </textarea>
+                </div>
 
                 <div className='images-title'>
                     Images
+                </div>
+
+                <div className='buttonchipsphotos-frame'>
+
+                    <div className='addphotos-frame'>
+                        <div className='addphotos-btn'>
+                            <PrimaryButton text={'Add Photos'}></PrimaryButton>
+                        </div>
+                    </div>
+
+                    <div className='nameimageanddelete-container noselect'>
+                        <div className='chipphoto-text'>
+                            image1.png
+                        </div>
+                        <div className='close-img'>
+                            <img src='images/xmark.svg'></img>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+                <Divider></Divider>
+
+                <Title title='Report Location'></Title>
+
+                <Label label='Address'></Label>
+                <InputText></InputText>
+
+                <div className='row-inputs'>
+                    <div className='label-input-country'>
+                        <Label label='Country'></Label>
+                        <InputText></InputText>
+                    </div>
+
+                    <div className='label-input-citydistrict'>
+                        <Label label='City / District'></Label>
+                        <InputText></InputText>
+                    </div>
+
+                    <div className='label-input-stateprovice'>
+                        <Label label='State / Provice'></Label>
+                        <InputText></InputText>
+                    </div>
+                </div>
+
+
+                <div className='register-btn-frame'>
+                    <div className='register-btn'>
+                        <PrimaryButton text={'Submit'}></PrimaryButton>
+                    </div>
                 </div>
 
             </div>
